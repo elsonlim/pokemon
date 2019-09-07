e84b5ca0f76ea399a686bfc2cf23d3faf6d0e6d9
Elysia Ong <elysia.owx@gmail.com>
2019-09-07 16:49:15 +0800

    Updates

---
+++ precache-manifest.0793362d1601f949803bbf16a8355afb.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9adfb647a53e3d7b456566efa3f0fd",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "426089310d0be3cd232c",
+    "url": "/pokemon/static/css/main.9753f76e.chunk.css"
+  },
+  {
+    "revision": "7d694c6e92c3ae4dff09",
+    "url": "/pokemon/static/js/2.02eb8440.chunk.js"
+  },
+  {
+    "revision": "426089310d0be3cd232c",
+    "url": "/pokemon/static/js/main.52637048.chunk.js"
+  },
+  {
+    "revision": "a3f4745a68544d3531ef",
+    "url": "/pokemon/static/js/runtime~main.fff94fa6.js"
+  },
+  {
+    "revision": "a58520a8b86897e148361db6b4ead562",
+    "url": "/pokemon/static/media/1.a58520a8.png"
+  },
+  {
+    "revision": "d9374649e18a3fc7169574b4030c56ea",
+    "url": "/pokemon/static/media/10.d9374649.png"
+  },
+  {
+    "revision": "1fd64989ce44e90504230848606bd5f1",
+    "url": "/pokemon/static/media/11.1fd64989.png"
+  },
+  {
+    "revision": "e57f8215ac0b402a86b812fd30b8566c",
+    "url": "/pokemon/static/media/12.e57f8215.png"
+  },
+  {
+    "revision": "cd2958fc20647d32617bf82f7810afd6",
+    "url": "/pokemon/static/media/13.cd2958fc.png"
+  },
+  {
+    "revision": "b20f84dd32345adee834ac61aafb2d84",
+    "url": "/pokemon/static/media/14.b20f84dd.png"
+  },
+  {
+    "revision": "200f96b2b97f371f21a4834576d477a9",
+    "url": "/pokemon/static/media/15.200f96b2.png"
+  },
+  {
+    "revision": "fa9d4d5623d1d93cf8d28126e5009f1e",
+    "url": "/pokemon/static/media/16.fa9d4d56.png"
+  },
+  {
+    "revision": "ab6fd53c5cddf5b056fc44c16653f13a",
+    "url": "/pokemon/static/media/17.ab6fd53c.png"
+  },
+  {
+    "revision": "878c76cba81f539b924f707353aa2722",
+    "url": "/pokemon/static/media/18.878c76cb.png"
+  },
+  {
+    "revision": "24530810b77b22fa067987523f57612c",
+    "url": "/pokemon/static/media/19.24530810.png"
+  },
+  {
+    "revision": "d5f2f1016776833008ce3031bc2d04a8",
+    "url": "/pokemon/static/media/2.d5f2f101.png"
+  },
+  {
+    "revision": "c3f945e4eb62d2739040c947a588c2b9",
+    "url": "/pokemon/static/media/20.c3f945e4.png"
+  },
+  {
+    "revision": "c4e1218f389a50aaa9c7e01cdb75a058",
+    "url": "/pokemon/static/media/21.c4e1218f.png"
+  },
+  {
+    "revision": "9ae3e8fe398ae7582acec95221e205c4",
+    "url": "/pokemon/static/media/22.9ae3e8fe.png"
+  },
+  {
+    "revision": "9a22a15167e7d8571306907c17669534",
+    "url": "/pokemon/static/media/23.9a22a151.png"
+  },
+  {
+    "revision": "cfbe281f4ccae41cf7a3a8d32fe597ee",
+    "url": "/pokemon/static/media/24.cfbe281f.png"
+  },
+  {
+    "revision": "3c4b41143f0c5f3d0cbe9aa65e7f4438",
+    "url": "/pokemon/static/media/25.3c4b4114.png"
+  },
+  {
+    "revision": "df7b8489ebfcec6f29045cbe0c4e9e70",
+    "url": "/pokemon/static/media/26.df7b8489.png"
+  },
+  {
+    "revision": "7cec4cf2db179bcc9c1adf454369fce6",
+    "url": "/pokemon/static/media/27.7cec4cf2.png"
+  },
+  {
+    "revision": "f7a23254043eb06401b1e662ab4bacc6",
+    "url": "/pokemon/static/media/28.f7a23254.png"
+  },
+  {
+    "revision": "f19dbd58e4839e07e787ca901567d31f",
+    "url": "/pokemon/static/media/29.f19dbd58.png"
+  },
+  {
+    "revision": "131f10bbf543f5fdbbb9e472ae1d50da",
+    "url": "/pokemon/static/media/3.131f10bb.png"
+  },
+  {
+    "revision": "b68e8ada98d14434e0280c29a595baa6",
+    "url": "/pokemon/static/media/30.b68e8ada.png"
+  },
+  {
+    "revision": "0c7813e84487dff5c9feb256c7f73108",
+    "url": "/pokemon/static/media/31.0c7813e8.png"
+  },
+  {
+    "revision": "092c6ce4928ce27134a24715284c2f36",
+    "url": "/pokemon/static/media/32.092c6ce4.png"
+  },
+  {
+    "revision": "3d9efd7a545b1b2e9e0313959a2d3bef",
+    "url": "/pokemon/static/media/33.3d9efd7a.png"
+  },
+  {
+    "revision": "a988d6fafc0954e02a54d9a14e572673",
+    "url": "/pokemon/static/media/34.a988d6fa.png"
+  },
+  {
+    "revision": "60e2921200a84661ec253101461a9994",
+    "url": "/pokemon/static/media/35.60e29212.png"
+  },
+  {
+    "revision": "5d128ac3e201ce3a80fc653dc3ce5c19",
+    "url": "/pokemon/static/media/36.5d128ac3.png"
+  },
+  {
+    "revision": "173ec2e778b4aa2c6667eee949441069",
+    "url": "/pokemon/static/media/37.173ec2e7.png"
+  },
+  {
+    "revision": "dc55882b498d04bc7b0bcc1ecfa10c7c",
+    "url": "/pokemon/static/media/38.dc55882b.png"
+  },
+  {
+    "revision": "2d9c934f64c9a5cd2a60194666940f28",
+    "url": "/pokemon/static/media/39.2d9c934f.png"
+  },
+  {
+    "revision": "f39948faa715049541b38c0a9e724458",
+    "url": "/pokemon/static/media/4.f39948fa.png"
+  },
+  {
+    "revision": "6965a3029031051991e786a4195858c4",
+    "url": "/pokemon/static/media/40.6965a302.png"
+  },
+  {
+    "revision": "9e71e9b76766fdfcec65441d5869baee",
+    "url": "/pokemon/static/media/41.9e71e9b7.png"
+  },
+  {
+    "revision": "575fc0b4b87cec3cb5a75138d36bf085",
+    "url": "/pokemon/static/media/42.575fc0b4.png"
+  },
+  {
+    "revision": "a9dc01e6b5a5baeb3cef42352576d382",
+    "url": "/pokemon/static/media/43.a9dc01e6.png"
+  },
+  {
+    "revision": "d262bf772acdbdc4980a0c0d225f0e58",
+    "url": "/pokemon/static/media/44.d262bf77.png"
+  },
+  {
+    "revision": "02326d3b0feb7acbfb537053d919453e",
+    "url": "/pokemon/static/media/45.02326d3b.png"
+  },
+  {
+    "revision": "afd90e5a744d1c0cfc4c80260820255f",
+    "url": "/pokemon/static/media/46.afd90e5a.png"
+  },
+  {
+    "revision": "617d15e7673f86fb00193ae6ad9206a0",
+    "url": "/pokemon/static/media/47.617d15e7.png"
+  },
+  {
+    "revision": "7924cbc5e2e85dc3c10509929f067026",
+    "url": "/pokemon/static/media/48.7924cbc5.png"
+  },
+  {
+    "revision": "df9e55f602c4d693d2fc176fe924a2a7",
+    "url": "/pokemon/static/media/49.df9e55f6.png"
+  },
+  {
+    "revision": "9d3548b27f541cab0279b149ee45dbf5",
+    "url": "/pokemon/static/media/5.9d3548b2.png"
+  },
+  {
+    "revision": "9c626449b4f8735370396d251f71a117",
+    "url": "/pokemon/static/media/50.9c626449.png"
+  },
+  {
+    "revision": "192b4a63e5fa7032155bf793b68d354f",
+    "url": "/pokemon/static/media/51.192b4a63.png"
+  },
+  {
+    "revision": "dfac0c2391578f589fbf352197a67ed9",
+    "url": "/pokemon/static/media/6.dfac0c23.png"
+  },
+  {
+    "revision": "4f719476241948328fa3ca2973fa3b87",
+    "url": "/pokemon/static/media/7.4f719476.png"
+  },
+  {
+    "revision": "558e0ac37cecf02472f330c51562964a",
+    "url": "/pokemon/static/media/8.558e0ac3.png"
+  },
+  {
+    "revision": "2a57c7e0d8160bdcf8dc557db550cdf9",
+    "url": "/pokemon/static/media/9.2a57c7e0.png"
+  }
+]);
