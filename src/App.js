da2123b0c1629fa644322184c7a3b2db0f4e8227
Elson Lim <elsonlim7@gmail.com>
2019-09-07 16:28:00 +0800

    Put everything together

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import POKEMON from './pokemon/pokemon';
+
+import Card from './card/Card';
 import './App.css';
 
 function App() {
+  const cards = POKEMON.map((pokemon, index) => <Card key={index} info={pokemon} />);
+
   return (
     <div className="App">
-      <header className="App-header">
-        MY POKEMON
-      </header>
+		<header className="AppHeader">
+			<h1>POKEMON</h1>
+		</header>
+		<div className="AppContainer">
+			{cards}
+		</div>
     </div>
   );
 }
