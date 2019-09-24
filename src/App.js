6f7a674ddb8197b2d827fbaf997886fd03871bf6
elsonlim <elsonlim7@gmail.com>
2019-09-24 16:26:07 +0800

    fetch pokemon image from url

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import POKEMON from './pokemon/pokemon';
+import React from "react";
+import POKEMON from "./pokemon/pokemon";
 
-import Card from './card/Card';
-import './App.css';
+import Card from "./card/Card";
+import "./App.css";
 
-function App() {
-  const cards = POKEMON.map((pokemon, index) => <Card key={index} info={pokemon} />);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="App">
-		<header className="AppHeader">
-			<h1>POKEMON</h1>
-		</header>
-		<div className="AppContainer">
-			{cards}
-		</div>
-    </div>
-  );
+    this.state = {
+      pokemonData: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://us-central1-pokedex-23fb6.cloudfunctions.net/app/pokemonData"
+    )
+      .then(res => res.json())
+      .then(resInJson =>
+        this.setState(state => {
+          return { pokemonData: resInJson };
+        })
+      );
+  }
+
+  render() {
+    const cards = this.state.pokemonData.map((pokemon, index) => (
+      <Card key={index} info={pokemon} />
+    ));
+
+    return (
+      <div className="App">
+        <header className="AppHeader">
+          <h1>POKEMON</h1>
+        </header>
+        <div className="AppContainer">{cards}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
